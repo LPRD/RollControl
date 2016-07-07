b2e510b70f50927345495210334a32f876e3afe6
Lucas Kramer <krame505@umn.edu>
2016-07-07 16:46:18 -0500

    Cleanup, all data logged and downlinked

--- RollControl/RollControl.ino
+++ RollControl/RollControl.ino
@@ -23,8 +23,8 @@ char filename[] = "DATA000.csv";
 
 #define servo1Pin  2
 #define servo2Pin  3
-#define loopDelay 200
-#define dataTime ((float)loopDelay)/1000      // time between data
+#define loopPeriod 200
+#define dataTime ((float)loopPeriod)/1000      // time between data
 #define gyro_size 10        // uses 0 : gyro_size-1 valid data points
 float gyro[gyro_size];
 int i = 0;
@@ -39,6 +39,21 @@ float rollProp;
 float rollInt;
 float rollDer;
 
+#define SEND_VECTOR_ITEM(field, value)  \
+  SEND_ITEM(x_##field, value.x())       \
+  SEND_ITEM(y_##field, value.y())       \
+  SEND_ITEM(z_##field, value.z())
+
+// Save global bytes by sharing the seperator string
+const char *seperator = ", ";
+
+#define WRITE_CSV_ITEM(value) \
+  dataFile.print(seperator); dataFile.print(value);
+#define WRITE_CSV_VECTOR_ITEM(value)  \
+  WRITE_CSV_ITEM(value.x())           \
+  WRITE_CSV_ITEM(value.y())           \
+  WRITE_CSV_ITEM(value.z())
+  
 
 void setup() {
   pinMode(1,OUTPUT);
@@ -63,7 +78,7 @@ void setup() {
     if (!SD.exists(filename))     // only open if file doesn't exist
     {
       dataFile = SD.open(filename, FILE_WRITE);
-      Serial.print("  writing to ");
+      Serial.print("writing ");
       Serial.println(filename);
       break;
     }
@@ -76,18 +91,23 @@ void setup() {
   delay(1000);
 }
 
-
-
 void loop() {
+  // Time length of loop to remain consistant
   long time0 = millis();
   i++;    if (i>=gyro_size)  { i = 0; }
+
+  // Get data
+  imu::Vector<3> magnetometer  = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  imu::Vector<3> gyroscope     = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  imu::Vector<3> euler         = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  imu::Vector<3> accelerometer = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  int8_t temp = bno.getTemp();
   
-  imu::Vector<3> gyroscope = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   gyro[i] = gyroscope.z();              // change dep. on orientation (x,y,z)
   
-  Serial.print("gyro.z :  ");
+/*  Serial.print("gyro.z :  ");
   Serial.print(gyro[i]);
-  Serial.println("");
+  Serial.println("");*/
   
   // Fixes reference errors with circular buffer
   b = i - 1;    if (b<1)  { b = b + gyro_size; }
@@ -116,26 +136,26 @@ void loop() {
         servo2.write(v + servo2Offset);
         
         // print angle components to serial
-        Serial.print  ("  90");
-        Serial.print  ("  +  ");
+        Serial.print  ("90");
+        Serial.print  ("+");
         Serial.print  (Kp*rollProp);
-        Serial.print  ("  +  ");
+        Serial.print  ("+");
         Serial.print  (Ki*rollInt);
-        Serial.print  ("  +  ");
+        Serial.print  ("+");
         Serial.print  (Kd*rollDer);
-        Serial.print  ("  =  ");
+        Serial.print  ("=");
         Serial.println(v);
   }
-/*
+
   // Downlink
-  sensors_event_t event; 
-  bno.getEvent(&event);
   BEGIN_SEND
-  SEND_ITEM(x_orientation, event.orientation.x);
-  SEND_ITEM(y_orientation, event.orientation.x);
-  SEND_ITEM(z_orientation, event.orientation.x);
+  SEND_ITEM(temperature, temp);
+  SEND_VECTOR_ITEM(magnetometer, magnetometer);
+  SEND_VECTOR_ITEM(gyro, gyroscope);
+  SEND_VECTOR_ITEM(euler_angle, euler);
+  SEND_VECTOR_ITEM(acceleration, accelerometer);
   END_SEND
-*/
+
 
   if (dataFile) {
     DateTime now = RTC.now();
@@ -144,12 +164,16 @@ void loop() {
     dataFile.print(now.day(), DEC);     dataFile.print(' ');
     dataFile.print(now.hour(), DEC);    dataFile.print(':');
     dataFile.print(now.minute(), DEC);  dataFile.print(':');
-    dataFile.print(now.second(), DEC);  dataFile.print(", ");
+    dataFile.print(now.second(), DEC);
+
+    WRITE_CSV_ITEM(temp)
+    WRITE_CSV_VECTOR_ITEM(magnetometer)
+    WRITE_CSV_VECTOR_ITEM(gyroscope)
+    WRITE_CSV_VECTOR_ITEM(euler)
+    WRITE_CSV_VECTOR_ITEM(accelerometer)
     
-    dataFile.print(gyro[i]);        dataFile.print(", ");
-    dataFile.print(gyroscope.y());  dataFile.print(", ");
-    dataFile.print(gyroscope.x());  dataFile.println("");
+    dataFile.println();
     dataFile.flush();
   }
-  delay(time0 + loopDelay - millis());     // continuously adjusted for desired dataTime
+  delay(time0 + loopPeriod - millis());     // continuously adjusted for desired dataTime
 }
